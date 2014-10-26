f2e110d5bf0eb1ac5ef3040888f2a46fe1392cd6
Yoga Hanggara <yogahanggara@gmail.com>
2014-10-26 15:01:09 +0700

    quick coding

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width">
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+    <div id="layout">
+        <!-- Menu toggle -->
+        <a href="#menu" id="menuLink" class="menu-link">
+            <!-- Hamburger icon -->
+            <span></span>
+        </a>
+
+        <div id="menu">
+            <div class="pure-menu pure-menu-open">
+                <a class="pure-menu-heading" href="#">yohang.net</a>
+
+                <ul>
+                    <li><a href="#">Home</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div id="main">
+            <div class="header">
+                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                <h2>Code is poetry</h2>
+            </div>
+
+
+            <?php if ( have_posts() ) : ?>
+                <?php while ( have_posts() ) : the_post(); ?>
+
+                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <div class="content">
+                            <header>
+                                <?php if ( is_single() ) : ?>
+
+                                <h1><?php the_title() ?></h1>
+
+                                <?php else: ?>
+
+                                <h1><a href="<?php echo esc_url( get_permalink() ) ?>"><?php the_title() ?></a></h1>
+
+                                <?php endif; ?>
+                            </header>
+
+                            <?php the_content() ?>
+
+                            <?php the_tags( '<footer><span class="tag-links">', '', '</span></footer>' ); ?>
+
+                        </div>
+
+
+                    </article>
+
+
+                <?php endwhile; ?>
+
+                <?php posts_nav_link(); ?>
+
+                <?php else : ?>
+
+                <?php endif; ?>
+
+
+        </div>
+    </div>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/ui.js"></script>
+    <?php wp_footer(); ?>
+</body>
+</html>
